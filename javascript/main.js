9d8d401e100af8ac4e57ff7cb2dce787ec04670e
Facundo Marin <marin.facu@gmail.com>
2021-01-25 20:41:04 -0300

    Ajustes Javascript, sume funciones

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,25 @@
-var precioCompra = 420.000
-var precioAlquilerDia = 8.000
-var precioEnvio = 1.500
+var precioCompra = 420000
+var precioAlquilerDia = 4500
+var precioEnvioCompra = 1500
+var precioEnvioAlquiler = 2500
+
+
 
 
 function solicitudProvincia(){
-    var provincia = (prompt("Ingresá tu provincia. Cordoba y Buenos Aires disponibles."));
+    var provincia = (prompt("Ingresá tu provincia. Cordoba y Buenos Aires disponibles.").toUpperCase());
     while(provincia != "Cordoba" && provincia != "cordoba" && provincia != "CORDOBA" && provincia != "buenos aires" && provincia != "BUENOS AIRES" && provincia != "Buenos Aires" && provincia != "Buenos aires"){
         alert("Por el momento solo contamos con distribuidores en Cordoba y Buenos Aires.");
-            var provincia = (prompt("Ingresá tu provincia. Cordoba y Buenos Aires disponibles."));
+            var provincia = (prompt("Ingresá tu provincia. Cordoba y Buenos Aires disponibles.").toUpperCase());
         }
     return provincia;
 }
 
-var provincia = solicitudProvincia();
-
-var alquilerCompra = (prompt("Ingresá alquiler o compra segun tu necesidad."));
-if(alquilerCompra == "alquiler" || alquilerCompra == "Alquiler" || alquilerCompra == "ALQUILER"){
-    var diasAlquiler = (prompt("Cuantos días necesitas alquilar?"));
-}
-
 function proveedor(){
-    if (provincia == "Cordoba" || provincia == "cordoba" || provincia == "CORDOBA"){
-        console.log ("Datos distribuidores en Cordoba");}
-        else if (provincia == "buenos aires" || provincia == "Buenos Aires" || provincia == "BUENOS AIRES"){
-        console.log ("Datos distribuidores en Buenos Aires")
+    if (provincia == "CORDOBA"){
+        console.log ("Distribuidores en Cordoba");}
+        else if (provincia == "BUENOS AIRES"){
+        console.log ("Distribuidores en Buenos Aires")
         }
 }
 
@@ -37,37 +33,51 @@ function Distribuidor(nombre, localidad, alquiler, venta, contacto, direccion, t
     this.telefono = telefono;
 }
 
-var Casa1 = new Distribuidor("Cordoba V-A", "Cordoba", "si", "si", "Jonas Germani", "Av. Colon 2774 - Cordoba capital", "351 428.6554");
-var Casa2 = new Distribuidor("Cordoba A", "Cordoba", "si", "no", "Joaquin Carbelo", "Pedro Zanni 48 - Cordoba capital", "351 442.4881");
-var Casa3 = new Distribuidor("Cordoba V", "Cordoba", "no", "si", "Florencia Rodriguez", "España 43 - Alta Gracia - Cordoba", "3547 411.2154");
-var Casa4 = new Distribuidor("Buenos Aires V-A", "Buenos Aires", "si", "si", "Juan Cruz Cabral", "Av. Brasil 2774 - CABA", "11 6302.3455");
-var Casa5 = new Distribuidor("Buenos Aires V", "Buenos Aires", "no", "si", "Federico Schorder", "Cochabamba 447 - CABA", "11 3369.4721");
-var Casa6 = new Distribuidor("Buenos Aires A", "Buenos Aires", "si", "no", "Fernando Salinas", "Av. Pedro Goyena 1982 - CABA", "11 8996.7426");
+var Casa1 = new Distribuidor("ENTER.", "Cordoba", "si", "si", "Jonas Germani", "Av. Colon 2774 - Cordoba capital", "351 428.6554");
+var Casa2 = new Distribuidor("Cocoon", "Cordoba", "si", "no", "Joaquin Carbelo", "Pedro Zanni 48 - Cordoba capital", "351 442.4881");
+var Casa3 = new Distribuidor("Lost & Found", "Cordoba", "no", "si", "Florencia Rodriguez", "España 43 - Alta Gracia - Cordoba", "3547 411.2154");
+var Casa4 = new Distribuidor("SudBeat", "Buenos Aires", "si", "si", "Juan Cruz Cabral", "Av. Brasil 2774 - CABA", "11 6302.3455");
+var Casa5 = new Distribuidor("Rumors", "Buenos Aires", "no", "si", "Federico Schorder", "Cochabamba 447 - CABA", "11 3369.4721");
+var Casa6 = new Distribuidor("Bedrock", "Buenos Aires", "si", "no", "Fernando Salinas", "Av. Pedro Goyena 1982 - CABA", "11 8996.7426");
 
 
 function distCordoba(){
-    if (provincia == "Cordoba" && alquilerCompra == "Alquiler"){
+    if (provincia == "CORDOBA" && alquilerCompra == "ALQUILER"){
         console.log (Casa1, Casa2);}
-    else if(provincia == "Cordoba" && alquilerCompra == "Venta"){
+    else if(provincia == "CORDOBA" && alquilerCompra == "COMPRA"){
         console.log (Casa1, Casa3);}
     }
 
 
 function distBuenosAires(){
-    if (provincia == "Buenos Aires" && alquilerCompra == "Alquiler"){
+    if (provincia == "BUENOS AIRES" && alquilerCompra == "ALQUILER"){
         console.log (Casa4, Casa6);}
-    else if(provincia == "Buenos Aires" && alquilerCompra == "Venta"){
+    else if(provincia == "BUENOS AIRES" && alquilerCompra == "COMPRA"){
         console.log (Casa4, Casa5);}
     }
 
+
+
+var provincia = solicitudProvincia();
+var alquilerCompra = (prompt("Ingresá alquiler o compra segun tu necesidad.").toUpperCase());
+if (alquilerCompra == "ALQUILER"){
+    var diasAlquiler = (prompt("Cuantos días necesitas alquilar?"));
+}
 
 var provedor = proveedor();
 distCordoba();
 distBuenosAires();
 
+var precioAlquiler = (diasAlquiler * precioAlquilerDia)
 
-
- //   else if(promedioFinal=>6){
-   // console.log("Promedio final: " + promedioFinal + ". APRUEBA.");
-   // }
-//}
+if (alquilerCompra == "COMPRA"){
+    console.log ("PRECIO DE COMPRA");
+    console.log ("Compra - sin envio: $" + precioCompra + ".-");
+    console.log ("Compra - con envio: $" + (precioCompra + precioEnvioCompra) + ".-");
+    }
+    else if (alquilerCompra == "ALQUILER"){
+            console.log ("PRECIO DE ALQUILER");
+            console.log ("Alquiler por " + diasAlquiler + "días - sin envio: $ " + precioAlquiler + ".-");
+            console.log ("Alquiler por " + diasAlquiler + "días - con envio: $ " + (precioAlquiler + precioEnvioAlquiler) + ".-");
+            console.log ("El costo de envio incluye retiro.");
+        }
